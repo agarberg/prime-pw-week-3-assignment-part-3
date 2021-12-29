04ae7fce3e1f8e89f9a2453894f85457de1cca3e
Adam Garberg <adamgarberg@gmail.com>
2021-12-29 14:37:08 -0600

    line48fixed

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -45,8 +45,8 @@ for (let i=0; i<supplyChanges.length; i++){
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
-for( Change of supplyChanges ){
-  console.log( Change );
+for( let change of supplyChanges ){
+  console.log( change );
 }
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
